122cbf063404b245eb6045ca4652c731e8074d46
Manuel_mma <manumaral04@gmail.com>
2024-11-22 13:42:37 +0100

    Registro Coordenada terminado

---
+++ src/main/java/org/iesalandalus/programacion/robot/modelo/Orientacion.java
@@ -0,0 +1,12 @@
+package org.iesalandalus.programacion.robot.modelo;
+
+public enum Orientacion {
+    NORTE,
+    NOROESTE,
+    NORESTE,
+    SUR,
+    SUROESTE,
+    SURESTE,
+    ESTE,
+    OESTE;
+}
