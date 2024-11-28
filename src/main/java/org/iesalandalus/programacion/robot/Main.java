327ce6700c21abed2d38bc9bcd510d84b9f5eb2e
Manuel_mma <manumaral04@gmail.com>
2024-11-28 18:25:01 +0100

    Consola terminada

--- src/main/java/org/iesalandalus/programacion/robot/Main.java
+++ src/main/java/org/iesalandalus/programacion/robot/Main.java
@@ -1,4 +1,76 @@
 package org.iesalandalus.programacion.robot;
-
+import org.iesalandalus.programacion.robot.modelo.*;
+import org.iesalandalus.programacion.utilidades.Entrada;
+import org.iesalandalus.programacion.robot.*;
 public class Main {
+
+    private ControladorRobot controladorRobot;
+
+    public static void main(String[] args) {
+
+
+        private static void ejecutarOpcion(int opcion){
+
+            System.out.println("Por favor elija una opción:");
+
+
+            switch (opcion) {
+                case 1:
+                    controlarRobotDefecto();
+                    break;
+                case 2:
+                    controlarRobotZona();
+                    break;
+                case 3:
+                    controlarRobotZonaOrientacion();
+                    break;
+                case 4:
+                    controlarRobotZonaOrientacionCoordenada();
+                    break;
+                case 5:
+                    ejecutarComando();
+                    break;
+                case 0:
+                    System.exit(0);
+                default:
+                    System.out.println("Opción no válida.");
+            }
+
+        }
+
+
+    }
+
+    private static void controlarRobotDefecto() {
+
+        Robot robot = new Robot();
+
+    }
+
+    private static void controlarRobotZona() {
+
+        Robot robot = new Robot(new Zona());
+
+    }
+
+    private static void controlarRobotZonaOrientacion() {
+
+        Robot robot = new Robot(new Zona(), Orientacion.NORTE);
+
+
+    }
+
+    private static void controlarRobotZonaOrientacionCoordenada() {
+
+        Robot robot = new Robot(new Zona(), Orientacion.NORTE, new Coordenada(0, 0));
+
+    }
+
+    private static void ejecutarComando() {
+
+
+        Robot robot = new Robot();
+
+        controladorRobot = new ControladorRobot(robot);
+    }
 }
--- src/main/java/org/iesalandalus/programacion/robot/vista/Consola.java
+++ src/main/java/org/iesalandalus/programacion/robot/vista/Consola.java
@@ -1,28 +1,31 @@
 package org.iesalandalus.programacion.robot.vista;
 
 
-import org.iesalandalus.programacion.robot.modelo.Robot;
-import org.iesalandalus.programacion.robot.modelo.Zona;
-import org.iesalandalus.programacion.robot.modelo.Orientacion;
-import org.iesalandalus.programacion.robot.modelo.Coordenada;
+import org.iesalandalus.programacion.robot.modelo.*;
 import org.iesalandalus.programacion.utilidades.Entrada;
 
 public class Consola {
 
     private Consola() {}
-
+/**1º CREAMOS EL CONSTRUCTOR QUE MOSTRARÁ EL MENU PRINCIPAL**/
     public static void mostrarMenuPrincipal() {
         System.out.println("Menú principal");
+
         System.out.println("1. Controlar robot por defecto");
+
         System.out.println("2. Controlar robot indicando su zona");
+
         System.out.println("3. Controlar robot indicando su zona y orientación");
+
         System.out.println("4. Controlar robot indicando su zona, orientación y coordenada inicial");
+
         System.out.println("5. Ejecutar comando");
+
         System.out.println("0. Salir");
     }
-
+/**2º CREAMOS EL METODO QUE ELIGA LA OPCIÓN DEL MENÚ**/
     public static int elegirOpcion() {
-        int opcion;
+        int opcion = -1;
         do {
             mostrarMenuPrincipal();
             System.out.println("Elija una opción del menú:");
@@ -30,7 +33,7 @@ public class Consola {
         } while (opcion < 0 || opcion > 5);
         return opcion;
     }
-
+/**3º CREAMOS EL CONSTRUCTOR QUE NOS DIRA QUE ANCHURA Y QUE ALTURA QUEREMOS PONER EN LA ZONA**/
     public static Zona elegirZona() {
         int ancho, alto;
         do {
@@ -41,42 +44,72 @@ public class Consola {
         } while (ancho < 10 || ancho > 100 || alto < 10 || alto > 100);
         return new Zona(ancho, alto);
     }
-
+/**4º CREAMOS EL CONSTRUCTOR QUE NOS MOSTRARÁ LAS ORIENTACIONES QUE HAY**/
     public static void mostrarMenuOrientacion() {
-        System.out.println("Orientaciones disponibles:");
-        for (Orientacion orientacion : Orientacion.values()) {
-            System.out.println(orientacion.ordinal() + ". " + orientacion);
-        }
-    }
+        System.out.println("Menú orientación");
 
+        System.out.println("1.Norte ");
+
+        System.out.println("2.Noreste ");
+
+        System.out.println("3.Este ");
+
+        System.out.println("4.Sureste");
+
+        System.out.println("5.Sur ");
+
+        System.out.println("6.Suroeste");
+
+        System.out.println("7.Oeste");
+
+        System.out.println("8.Noroeste");
+
+
+    }
+/**5º CREAMOS EL METODO QUE ELIGA LA ORIENTACIÓN DEL ROBOT**/
     public static Orientacion elegirOrientacion() {
         int opcion;
         do {
             mostrarMenuOrientacion();
             System.out.println("Elija una orientación:");
             opcion = Entrada.entero();
-        } while (opcion < 0 || opcion >= Orientacion.values().length);
-        return Orientacion.values()[opcion];
+        } while (opcion < 1 || opcion >8);
+        switch (opcion){
+            case 1: return Orientacion.NORTE;
+            case 2: return Orientacion.NOROESTE;
+            case 3: return Orientacion.ESTE;
+            case 4: return Orientacion.SURESTE;
+            case 5: return Orientacion.SUR;
+            case 6: return Orientacion.SUROESTE;
+            case 7: return Orientacion.OESTE;
+            case 8: return Orientacion.NOROESTE;
+            default: return null;
+        }
     }
-
+/**6º CREAMOS EL CONSTRUCTOR QUE NOS DIRA QUE AÑADAMOS LAS COORDENADAS**/
     public static Coordenada elegirCoordenada() {
-        System.out.println("Introduzca la componente X de la coordenada:");
+        System.out.println("Introduzca el punto X de la coordenada:");
         int x = Entrada.entero();
-        System.out.println("Introduzca la componente Y de la coordenada:");
+        System.out.println("Introduzca el punto Y de la coordenada:");
         int y = Entrada.entero();
         return new Coordenada(x, y);
     }
+/**7º CREAMOS EL METODO QUE NOS DIRA QUE COMANDO QUEREMOS EJECUTAR**/
+    public static char elegirComando() {
+        char comando;
+        do {
 
-    public static String elegirComando() {
-        System.out.println("Introduzca el comando a ejecutar:");
-        return Entrada.cadena();
+            System.out.println("Elija una opción del menú:");
+            comando = Entrada.caracter();
+        } while (Character.toLowerCase(comando) != 'a' && Character.toLowerCase(comando) != 'd' && Character.toLowerCase(comando) != 'i'  );
+        return comando;
     }
 
-    public static void mostrarRobot(Robot robot) {
-        if (robot == null) {
+    public static void mostrarRobot(ControladorRobot controladorRobot) {
+        if (controladorRobot == null) {
             System.out.println("El robot es nulo.");
         } else {
-            System.out.println(robot);
+            System.out.println(controladorRobot.getRobot().toString());
         }
     }
 
