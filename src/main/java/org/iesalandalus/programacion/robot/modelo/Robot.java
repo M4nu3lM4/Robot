e7c7791bfdae3d4996208fb816d104c4c4a458d1
Manuel_mma <manumaral04@gmail.com>
2025-11-21 18:58:08 +0100

    Clase Robot terminada y test superados

--- src/main/java/org/iesalandalus/programacion/robot/modelo/Robot.java
+++ src/main/java/org/iesalandalus/programacion/robot/modelo/Robot.java
@@ -9,26 +9,42 @@ public class Robot {
 
 
     public Robot(){
-        Robot robot = new Robot();
+        zona = new Zona();
+        orientacion = Orientacion.NORTE;
+        coordenada = zona.getCentro();
+
     }
     public Robot(Zona zona){
-        Objects.requireNonNull(zona,"La zona no puede ser nula");
+        Objects.requireNonNull(zona,"La zona no puede ser nula.");
         setZona(zona);
+        setOrientacion(Orientacion.NORTE);
+        coordenada = zona.getCentro();
+
     }
 
     public Robot(Zona zona, Orientacion orientacion){
         Objects.requireNonNull(zona,"La zona no puede ser nula.");
+
         Objects.requireNonNull(orientacion,"La orientación no puede ser nula.");
+
         setZona(zona);
+
         setOrientacion(orientacion);
+
+        coordenada = zona.getCentro();
     }
 
     public Robot(Zona zona, Orientacion orientacion, Coordenada coordenada){
         Objects.requireNonNull(zona,"La zona no puede ser nula.");
+
         Objects.requireNonNull(orientacion,"La orientación no puede ser nula.");
+
         Objects.requireNonNull(coordenada,"La coordenada no puede ser nula.");
+
         setZona(zona);
+
         setOrientacion(orientacion);
+
         setCoordenada(coordenada);
     }
 
@@ -36,7 +52,9 @@ public class Robot {
         Objects.requireNonNull(robot,"El robot no puede ser nulo.");
 
         zona = robot.zona;
+
         orientacion = robot.orientacion;
+
         coordenada = robot.coordenada;
 
     }
@@ -61,49 +79,87 @@ public class Robot {
         return coordenada;
     }
 
-    public void setCoordenada(Coordenada coordenada) {
+    public void setCoordenada(Coordenada coordenada)throws IllegalArgumentException {
         this.coordenada = Objects.requireNonNull(coordenada,"La coordenada no puede ser nula.");
         if (this.zona.pertenece(coordenada)){
             this.coordenada = coordenada;
+        }else {
+            throw new IllegalArgumentException("La coordenada no pertenece a la zona.");
         }
     }
 
     public void avanzar()throws RobotExcepcion{
-         Coordenada coordenadaAux = new Coordenada(coordenada.x(), coordenada.y());
 
-
+        Coordenada coordenadaAux = new Coordenada(coordenada.x(), coordenada.y());
 
         try{
             switch (getOrientacion()){
 
-                case NORTE -> coordenadaAux = new Coordenada(coordenada.x(),coordenada.y()+1);
 
-                case NORESTE -> coordenadaAux = new Coordenada(coordenada.x() + 1,coordenada.y() + 1);
+                case NORTE -> coordenadaAux = new Coordenada(coordenada.x(),coordenada.y() + 1);
 
-                case ESTE -> coordenadaAux = new Coordenada(coordenada.x() +1, coordenada.y() );
+                case NORESTE -> coordenadaAux = new Coordenada(coordenada.x() + 1 ,coordenada.y() + 1);
 
-                case SURESTE -> coordenadaAux = new Coordenada(coordenada.x() + 1,coordenada.y() - 1);
+                case ESTE -> coordenadaAux = new Coordenada(coordenada.x() + 1 , coordenada.y() );
+
+                case SURESTE -> coordenadaAux = new Coordenada(coordenada.x() + 1 ,coordenada.y() - 1);
 
                 case SUR -> coordenadaAux = new Coordenada(coordenada.x(), coordenada.y() - 1);
 
-                case SUROESTE -> coordenadaAux = new Coordenada(coordenada.x() - 1, coordenada.y() - 1);
+                case SUROESTE -> coordenadaAux = new Coordenada(coordenada.x() - 1 , coordenada.y() - 1);
 
-                case OESTE -> coordenadaAux = new Coordenada(coordenada.x() - 1, coordenada.y());
+                case OESTE -> coordenadaAux = new Coordenada(coordenada.x() - 1 , coordenada.y());
 
-                case NOROESTE -> coordenadaAux = new Coordenada(coordenada.x() - 1, coordenada.y() + 1);
+                case NOROESTE -> coordenadaAux = new Coordenada(coordenada.x() - 1 , coordenada.y() + 1);
+
+                default -> throw new RobotExcepcion("No se puede avanzar, ya que se sale de la zona.");
 
             }
+            if (zona.pertenece(coordenadaAux)){
+                setCoordenada(coordenadaAux);
+            }else {
+                throw new RobotExcepcion("No se puede avanzar, ya que se sale de la zona.");
+            }
+
         }catch (RobotExcepcion e){
-            throw new RobotExcepcion("El robot se ha pasado de los limites.");
+            throw new RobotExcepcion("No se puede avanzar, ya que se sale de la zona.");
         }
     }
 
-    public void girarALaDerecha(){
+    public void girarALaDerecha()throws RobotExcepcion{
+        try{
+            switch (getOrientacion()){
+                case NORTE -> setOrientacion(Orientacion.NORESTE);
+                case NORESTE -> setOrientacion(Orientacion.ESTE);
+                case ESTE -> setOrientacion(Orientacion.SURESTE);
+                case SURESTE -> setOrientacion(Orientacion.SUR);
+                case SUR -> setOrientacion(Orientacion.SUROESTE);
+                case SUROESTE -> setOrientacion(Orientacion.OESTE);
+                case OESTE -> setOrientacion(Orientacion.NOROESTE);
+                case NOROESTE -> setOrientacion(Orientacion.NORTE);
+            }
 
+        }catch (RobotExcepcion e){
+            throw new RobotExcepcion("No se puede realizar el giro");
+        }
     }
 
     public void girarALaIzquierda(){
+        try{
+            switch (getOrientacion()){
+                case NORTE -> setOrientacion(Orientacion.NOROESTE);
+                case NOROESTE -> setOrientacion(Orientacion.OESTE);
+                case OESTE -> setOrientacion(Orientacion.SUROESTE);
+                case SUROESTE -> setOrientacion(Orientacion.SUR);
+                case SUR -> setOrientacion(Orientacion.SURESTE);
+                case SURESTE -> setOrientacion(Orientacion.ESTE);
+                case ESTE -> setOrientacion(Orientacion.NORESTE);
+                case NORESTE -> setOrientacion(Orientacion.NORTE);
+            }
 
+        }catch (RobotExcepcion e){
+            throw new RobotExcepcion("No se puede realizar el giro.");
+        }
     }
 
     @Override
@@ -120,10 +176,10 @@ public class Robot {
 
     @Override
     public String toString() {
-        return "Robot{" +
+        return "Robot[" +
                 "zona=" + zona +
                 ", coordenada=" + coordenada +
                 ", orientacion=" + orientacion +
-                '}';
+                ']';
     }
 }
